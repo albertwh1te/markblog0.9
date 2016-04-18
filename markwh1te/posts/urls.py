abb2f95f07ce6e1c2f5972a7ddcac8a6e6215e6e
mark <iamwh1temark@outlook.com>
2016-04-18 19:01:09 +0800

    post_list

--- markwh1te/posts/urls.py
+++ markwh1te/posts/urls.py
@@ -2,6 +2,7 @@
 from django.conf.urls import url
 import views
 urlpatterns = [
-    url(r'^posts/$', views.posts_list.as_view(), name='posts_list'),
+    url(r'^$', views.Posts_list.as_view(), name='posts_list'),
+    url(r'^(?P<pk>\d+)/$', views.Posts_detail.as_view(), name="post_detail")
     # url(r'^tags/$', views.tags_list.as_view(), name='tags_list'),
     ]
--- markwh1te/posts/views.py
+++ markwh1te/posts/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 import models
 # Create your views here.
 
 
-class posts_list(ListView):
+class Posts_list(ListView):
+    model = models.Posts
+
+
+class Posts_detail(DetailView):
     model = models.Posts
