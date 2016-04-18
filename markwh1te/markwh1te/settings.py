d748ba24b89b0f8baebfe32bfb3c70e97e1afd42
mark <iamwh1temark@outlook.com>
2016-04-18 20:53:50 +0800

    bootstrap

--- markwh1te/markwh1te/settings.py
+++ markwh1te/markwh1te/settings.py
@@ -102,4 +102,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+        MEDIA_ROOT,
+        BASE_DIR,
+        )
+    
