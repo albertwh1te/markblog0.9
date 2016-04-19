4a7ed7130d15550c357ee315de0bc36eebd9752d
mark <iamwh1temark@outlook.com>
2016-04-19 19:30:48 +0800

    basic function finished

--- markwh1te/posts/urls.py
+++ markwh1te/posts/urls.py
@@ -1,8 +1,10 @@
 # coding=utf-8
 from django.conf.urls import url
+from django.views.generic import TemplateView
 import views
 urlpatterns = [
     url(r'^$', views.Posts_list.as_view(), name='posts_list'),
-    url(r'^(?P<pk>\d+)/$', views.Posts_detail.as_view(), name="post_detail")
+    url(r'^(?P<pk>\d+)/$', views.Posts_detail.as_view(), name="posts_detail"),
+    url(r'^about/$', TemplateView.as_view(template_name="about.html"),name="about"),
     # url(r'^tags/$', views.tags_list.as_view(), name='tags_list'),
     ]
