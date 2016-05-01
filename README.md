453344b1aa5b41b6f5d4188570aded0fad0bc982
mark <iamwh1temark@outlook.com>
2016-05-01 13:26:50 +0800

    update gitignore

--- README.md
+++
@@ -1,2 +0,0 @@
-# 本博客基于Django + Bootstrap,支持markdown，响应式。　
-
--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/mark/git/markblog0.9/env/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
