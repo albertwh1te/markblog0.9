5bfad31274c6dd5d4c657ccf895c9d30aa779d1f
MarkWh1te <iammarkwh1te@outlook.com>
2016-06-15 04:08:29 -0400

    update deploy

--- markwh1te/markwh1te/settings.py
+++ markwh1te/markwh1te/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z+((6r2#4h$y@4n)bx_8s%*yk&4wsgg8d_91^$(2zo3oj)cxp8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -104,9 +104,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
-        MEDIA_ROOT,
-        BASE_DIR,
-        )
-    
+MEDIA_ROOT = os.path.join(BASE_DIR, 'markwh1te')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATICFILES_DIRS = (
+#        MEDIA_ROOT,
+#        BASE_DIR,
